247a72c42a3bfd1ce3e13378dfc797c6de11ab81
Joseph Lionarons <lionsamurai@gmail.com>
2019-01-02 00:01:59 -0500

    smoothing sticky navbar

--- stickyheader.js
+++ stickyheader.js
@@ -1,14 +1,14 @@
 window.onscroll = function () {
-  setSticky();
+  fixHeader();
 };
 
 const navbar = document.getElementById('nav-header');
 const offsetAmount = navbar.offsetTop;
 
-function setSticky() {
+function fixHeader() {
   if (window.pageYOffset > offsetAmount) {
-    navbar.classList.add('sticky');
+    navbar.classList.add('fixed');
   } else {
-    navbar.classList.remove('sticky');
+    navbar.classList.remove('fixed');
   }
 }
