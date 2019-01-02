841f395ae34a0405c0cfe9637bac161aafbd157f
Joseph Lionarons <lionsamurai@gmail.com>
2019-01-02 12:14:08 -0500

    addded section IDs

--- stickyheader.js
+++ stickyheader.js
@@ -5,7 +5,7 @@ window.onscroll = function () {
 const navbar = document.getElementById('nav-header');
 
 function fixHeader() {
-  if (window.pageYOffset > (window.innerHeight * 0.92)) {
+  if (window.pageYOffset > window.innerHeight * 0.92) {
     navbar.classList.add('fixed');
   } else {
     navbar.classList.remove('fixed');
