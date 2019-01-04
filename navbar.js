148c5ed5a5aa1b50c333b15602aef03b0c7801ef
Joseph Lionarons <lionsamurai@gmail.com>
2019-01-04 10:27:20 -0500

    improved navbar functionality

--- navbar.js
+++ navbar.js
@@ -23,22 +23,22 @@ const pages = document.getElementsByClassName('page');
 const navLinks = document.getElementsByClassName('nav-link');
 
 function assignActiveNavLink() {
-  if (window.pageYOffset >= pages[4].offsetTop - pageHeight / 8) {
+  if (window.pageYOffset >= pages[4].offsetTop - pageHeight / 2) {
     resetActive();
     navLinks[4].classList.add('active');
-  } else if (window.pageYOffset >= pages[3].offsetTop - pageHeight / 8) {
+  } else if (window.pageYOffset >= pages[3].offsetTop - pageHeight / 2) {
     resetActive();
     navLinks[3].classList.add('active');
-  } else if (window.pageYOffset >= pages[2].offsetTop - pageHeight / 8) {
+  } else if (window.pageYOffset >= pages[2].offsetTop - pageHeight / 2) {
     resetActive();
     navLinks[2].classList.add('active');
-  } else if (window.pageYOffset >= pages[1].offsetTop - pageHeight / 8) {
+  } else if (window.pageYOffset >= pages[1].offsetTop - pageHeight / 2) {
     resetActive();
     navLinks[1].classList.add('active');
   } else {
     resetActive();
     navLinks[0].classList.add('active');
-    if (window.pageYOffset <= pageHeight / 4 && leftFirstPage) {
+    if (window.pageYOffset <= pageHeight / 8 && leftFirstPage) {
       $('#header-container').transition('bounce');
       leftFirstPage = false;
     }
