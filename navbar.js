142f645071c0bc35a0d22a709a6b0cd51e711b3f
Joseph Lionarons <lionsamurai@gmail.com>
2019-01-05 15:39:00 -0500

    modals structured

--- navbar.js
+++ navbar.js
@@ -25,19 +25,19 @@ const navLinks = document.getElementsByClassName('nav-link');
 function assignActiveNavLink() {
   if (window.pageYOffset >= pages[4].offsetTop - pageHeight / 2) {
     resetActive();
-    navLinks[4].classList.add('active');
+    navLinks[4].classList.add('nav-active');
   } else if (window.pageYOffset >= pages[3].offsetTop - pageHeight / 2) {
     resetActive();
-    navLinks[3].classList.add('active');
+    navLinks[3].classList.add('nav-active');
   } else if (window.pageYOffset >= pages[2].offsetTop - pageHeight / 2) {
     resetActive();
-    navLinks[2].classList.add('active');
+    navLinks[2].classList.add('nav-active');
   } else if (window.pageYOffset >= pages[1].offsetTop - pageHeight / 2) {
     resetActive();
-    navLinks[1].classList.add('active');
+    navLinks[1].classList.add('nav-active');
   } else {
     resetActive();
-    navLinks[0].classList.add('active');
+    navLinks[0].classList.add('nav-active');
     if (window.pageYOffset <= pageHeight / 8 && leftFirstPage) {
       $('#header-container').transition('bounce');
       leftFirstPage = false;
@@ -47,6 +47,6 @@ function assignActiveNavLink() {
 
 function resetActive() {
   for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].classList.remove('active');
+    navLinks[i].classList.remove('nav-active');
   }
 }
