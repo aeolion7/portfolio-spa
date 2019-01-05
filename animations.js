3805a3e12251bf21e625683152d5d21380a54892
Joseph Lionarons <lionsamurai@gmail.com>
2019-01-05 11:36:52 -0500

    set timeout before animations

--- animations.js
+++ animations.js
@@ -1,9 +1,11 @@
-$('#name').transition({
-  animation: 'scale',
-  duration: '500ms',
-  onComplete: function () {
-    $('.nav-link').transition('fly right', '1s');
-  }
-});
+window.setTimeout(function () {
+  $('#name').transition({
+    animation: 'scale',
+    duration: '500ms',
+    onComplete: function () {
+      $('.nav-link').transition('fly right', '1s');
+    },
+  });
 
-$('#header-title').transition('scale', '1s');
+  $('#header-title').transition('scale', '1s');
+}, 500);
