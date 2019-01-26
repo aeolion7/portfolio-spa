f65c329f0333c9fbf4118b462c7340393755330e
Joseph Lionarons <lionsamurai@gmail.com>
2019-01-26 17:00:32 -0500

    removed unnecessary commented code

--- server/index.js
+++ server/index.js
@@ -11,8 +11,6 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use('/api', require('./api'));
-
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
